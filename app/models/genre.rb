9d79f01433c0f9d493a4687bbb7dd55658c3b117
github username <github email address>
2019-10-26 17:20:37 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,18 @@
 class Genre < ActiveRecord::Base
     has_many :songs
     has_many :artists, through: :songs
+
+    def song_count
+        self.songs.count
+    end
+
+    def artist_count
+        self.artists.count 
+    end
+
+    def all_artist_names
+        self.artists.names
+    end
+
+    
 end
